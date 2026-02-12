32aa30fee90d6f10c32460b55223260b919db109
Bui Huu Tai <huutaiii2410@gmail.com>
2026-02-12 08:59:32 +0700

    modify db_init.py to create limits.json in data folder

--- backend/db_init.py
+++ backend/db_init.py
@@ -1,7 +1,10 @@
 import os
 import pandas as pd
+import json
 
-DATA_PATH = os.path.join(os.path.dirname(__file__), '..', 'data', 'expenses.csv')
+DATA_DIR = os.path.join(os.path.dirname(__file__), '..', 'data')
+EXPENSE_PATH = os.path.join(DATA_DIR,'expenses.csv')
+LIMIT_PATH = os.path.join(DATA_DIR, 'limits.json')
 
 COLUMNS = [
     "id",          # uuid
@@ -10,14 +13,36 @@ COLUMNS = [
     "amount",      # float
     "category"     # str
 ]
+CATEGORIES = {
+    "Food" : 0,
+    "Transport" : 0,
+    "Entertainment" :0,
+    "Shopping" : 0,
+    "Traveling" : 0,
+    "Utilities" : 0,
+    "Other" :0
+}
 
 def init_db():
-    if not os.path.exists(DATA_PATH):
+    # create data folder if not exists
+    if not os.path.exists(DATA_DIR):
+        os.makedirs(DATA_DIR)
+        print("data folder created")
+    # create expenses.csv
+    if not os.path.exists(EXPENSE_PATH):
         df = pd.DataFrame(columns=COLUMNS)
-        df.to_csv(DATA_PATH, index=False)
+        df.to_csv(EXPENSE_PATH, index=False)
         print("expenses.csv created with headers")
     else:
         print("expenses.csv already exists")
+    # create limits.json
+    if not os.path.exists(LIMIT_PATH):
+        with open(LIMIT_PATH,'w') as f:
+            json.dump(CATEGORIES,f)
+            print("limits.json created")
+    else:
+        print("limits.json already exists")
+
 
 if __name__ == "__main__":
     init_db()
