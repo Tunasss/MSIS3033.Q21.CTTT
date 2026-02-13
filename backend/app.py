134bd4391f92dc9b4b8ccf1da8359b9ed5f3ba5e
Bui Huu Tai <huutaiii2410@gmail.com>
2026-02-13 23:15:36 +0700

    integrate with front-end

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template  
 from flask_cors import CORS
 import pandas as pd
 import os
@@ -58,7 +58,7 @@ def load_limits():
 
 @app.route('/')
 def home():
-    return jsonify({"message": "Expense Tracker API", "status": "running"})
+    return render_template("index.html")
 
 @app.route('/api/expenses', methods=['GET'])
 def get_expenses():
